8fe1716d38b6452684d66ded0c2083b12c3072fa
Zachary Canann <zacharycanann@gmail.com>
2026-05-13 11:17:47 -0700

    Add contained call value fail canary

--- compiler/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/orchestration/omega-compiler/tests/canary_suite.rs
@@ -112,5 +112,6 @@ const ACTIVE_FAIL_CANARIES: &[&str] = &[
     "bounded_match_guard_unproven",
     "runtime_arithmetic_guard",
     "runtime_call_guard",
+    "runtime_contained_call_value",
     "runtime_contained_range_write",
 ];
