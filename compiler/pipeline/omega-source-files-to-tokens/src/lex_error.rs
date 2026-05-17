1058bc2ecc178e8a760ef356ecdebd764f34087e
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 09:11:53 -0700

    Share frontend error message storage

--- compiler/pipeline/omega-source-files-to-tokens/src/lex_error.rs
+++ compiler/pipeline/omega-source-files-to-tokens/src/lex_error.rs
@@ -1,15 +1,16 @@
 use omega_core::Span;
+use std::sync::Arc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct LexError {
-    pub message: String,
+    pub message: Arc<str>,
     pub span: Span,
 }
 
 impl LexError {
     pub fn new(message: impl Into<String>, span: Span) -> Self {
         Self {
-            message: message.into(),
+            message: Arc::from(message.into().into_boxed_str()),
             span,
         }
     }
--- compiler/pipeline/omega-tokens-to-syntax-trees/src/parse_error.rs
+++ compiler/pipeline/omega-tokens-to-syntax-trees/src/parse_error.rs
@@ -1,22 +1,23 @@
 use omega_core::source::SourceSpan;
+use std::sync::Arc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ParseError {
-    pub message: String,
+    pub message: Arc<str>,
     pub source_span: SourceSpan,
 }
 
 impl ParseError {
     pub fn new(message: impl Into<String>) -> Self {
         Self {
-            message: message.into(),
+            message: Arc::from(message.into().into_boxed_str()),
             source_span: SourceSpan::default(),
         }
     }
 
     pub fn at_source_span(message: impl Into<String>, source_span: SourceSpan) -> Self {
         Self {
-            message: message.into(),
+            message: Arc::from(message.into().into_boxed_str()),
             source_span,
         }
     }
