08dddb531c3b1dd34b6344e4ded82938538d21d3
Zachary Canann <zacharycanann@gmail.com>
2026-05-21 22:57:43 -0700

    Keep graph nodes stable for double click

--- compiler/orchestration/omega-visualizations/src/phase_diagram.rs
+++ compiler/orchestration/omega-visualizations/src/phase_diagram.rs
@@ -1173,7 +1173,7 @@ function drawNode(node) {
   });
   group.addEventListener("click", event => {
     event.stopPropagation();
-    selectNode(node.id, true);
+    selectNode(node.id, false);
   });
   group.addEventListener("dblclick", event => {
     event.stopPropagation();
