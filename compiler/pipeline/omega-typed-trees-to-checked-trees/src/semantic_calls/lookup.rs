61d1564ece41131e3783901df933b1682eb05048
Zachary Canann <zacharycanann@gmail.com>
2026-05-23 16:41:10 -0700

    Split checked-tree semantic call lookup

---
+++ compiler/pipeline/omega-typed-trees-to-checked-trees/src/semantic_calls/lookup.rs
@@ -0,0 +1,38 @@
+use super::*;
+
+pub(crate) fn call_site_argument_expressions<'program>(
+    program: &'program omega_typed_trees::TypedTrees,
+    call_site: &CallSite<'program>,
+) -> &'program [ExpressionHandle] {
+    match call_site {
+        CallSite::Statement(call) => program.statement_table.expression_handles(call.arguments),
+        CallSite::Expression(call) => program.expression_table.expression_handles(call.arguments),
+    }
+}
+
+pub(crate) fn find_state_in_machine<'program>(
+    program: &'program omega_typed_trees::TypedTrees,
+    machine_symbol: SymbolHandle,
+    state_symbol: SymbolHandle,
+) -> Option<&'program omega_typed_trees::state::State> {
+    let machine = program
+        .machines()
+        .iter()
+        .find(|machine| machine.symbol == machine_symbol)?;
+    program
+        .machine_states(machine)
+        .iter()
+        .find(|state| state.symbol == state_symbol)
+}
+
+pub(crate) fn find_state<'program>(
+    program: &'program omega_typed_trees::TypedTrees,
+    state_symbol: SymbolHandle,
+) -> Option<&'program omega_typed_trees::state::State> {
+    program.machines().iter().find_map(|machine| {
+        program
+            .machine_states(machine)
+            .iter()
+            .find(|state| state.symbol == state_symbol)
+    })
+}
