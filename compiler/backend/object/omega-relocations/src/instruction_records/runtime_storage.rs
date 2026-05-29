25ed6b2d63485b096446d9a56b1ed0b2dab891ee
Zachary Canann <zacharycanann@gmail.com>
2026-05-28 21:03:53 -0700

    Split runtime storage string relocations

--- compiler/backend/object/omega-relocations/src/instruction_records/runtime_storage.rs
+++ compiler/backend/object/omega-relocations/src/instruction_records/runtime_storage.rs
@@ -1,16 +1,13 @@
 use super::super::offsets::{
-    runtime_frame_indexed_string_data_address_offset,
-    runtime_machine_indexed_string_data_address_offset,
-    runtime_machine_indexed_string_runtime_frame_address_offset,
     runtime_storage_compare_right_address_offset,
     runtime_storage_copy_from_runtime_frame_fixed_indexed_target_address_offset,
     runtime_storage_copy_from_runtime_frame_indexed_target_address_offset,
     runtime_storage_copy_from_runtime_machine_indexed_runtime_frame_address_offset,
     runtime_storage_copy_from_runtime_machine_indexed_target_address_offset,
-    runtime_storage_copy_target_address_offset, string_descriptor_machine_address_offset,
-    string_descriptor_pointee_address_offset,
+    runtime_storage_copy_target_address_offset,
 };
 use super::context::InstructionRelocationContext;
+use super::runtime_storage_strings;
 use super::runtime_values::collect_runtime_value_operand_relocations;
 use omega_instruction_selection::runtime_machine_indexed_integer_runtime_frame_address_offset;
 use omega_target::Architecture;
@@ -20,6 +17,10 @@ pub(super) fn collect_runtime_storage_relocations(
     context: &mut InstructionRelocationContext<'_, '_>,
     instruction: &SelectedInstructionKind,
 ) -> bool {
+    if runtime_storage_strings::collect_runtime_storage_string_relocations(context, instruction) {
+        return true;
+    }
+
     match instruction {
         SelectedInstructionKind::EvaluateDispatchGuard {
             guard_lowering: StateGuardLowering::CompareStaticValue,
@@ -202,70 +203,6 @@ pub(super) fn collect_runtime_storage_relocations(
                 *left,
             );
             collect_runtime_value_operand_relocations(context, left_offset + left_width, *right);
-            true
-        }
-        SelectedInstructionKind::WriteRuntimeMachineString { data, .. } => {
-            let data_symbol = context.data_object_symbol_handle(*data);
-            context.insert_data_address_at_instruction_start(data_symbol);
-            context.insert_data_address_at_relative_offset(
-                string_descriptor_machine_address_offset(context.input.target.architecture),
-                context.machine_storage_symbol_handle(),
-            );
-            true
-        }
-        SelectedInstructionKind::WriteRuntimePointeeString { data, .. } => {
-            let data_symbol = context.data_object_symbol_handle(*data);
-            context.insert_data_address_at_instruction_start(data_symbol);
-            context.insert_data_address_at_relative_offset(
-                string_descriptor_pointee_address_offset(context.input.target.architecture),
-                context.runtime_frame_symbol_handle(),
-            );
-            true
-        }
-        SelectedInstructionKind::WriteRuntimeFrameIndexedString {
-            data,
-            element_byte_size,
-            field_byte_offset,
-            ..
-        } => {
-            context.insert_data_address_at_instruction_start(context.runtime_frame_symbol_handle());
-            let data_symbol = context.data_object_symbol_handle(*data);
-            context.insert_data_address_at_relative_offset(
-                runtime_frame_indexed_string_data_address_offset(
-                    context.input.target.architecture,
-                    *element_byte_size,
-                    *field_byte_offset,
-                ),
-                data_symbol,
-            );
-            true
-        }
-        SelectedInstructionKind::WriteRuntimeMachineIndexedString {
-            data,
-            base_byte_offset,
-            element_byte_size,
-            field_byte_offset,
-            ..
-        } => {
-            context
-                .insert_data_address_at_instruction_start(context.machine_storage_symbol_handle());
-            context.insert_data_address_at_relative_offset(
-                runtime_machine_indexed_string_runtime_frame_address_offset(
-                    context.input.target.architecture,
-                    *base_byte_offset,
-                ),
-                context.runtime_frame_symbol_handle(),
-            );
-            let data_symbol = context.data_object_symbol_handle(*data);
-            context.insert_data_address_at_relative_offset(
-                runtime_machine_indexed_string_data_address_offset(
-                    context.input.target.architecture,
-                    *base_byte_offset,
-                    *element_byte_size,
-                    *field_byte_offset,
-                ),
-                data_symbol,
-            );
             true
         }
         SelectedInstructionKind::WriteRuntimeStorageAddressToRuntimeFrame {
---
+++ compiler/backend/object/omega-relocations/src/instruction_records/runtime_storage_strings.rs
@@ -0,0 +1,81 @@
+use super::super::offsets::{
+    runtime_frame_indexed_string_data_address_offset,
+    runtime_machine_indexed_string_data_address_offset,
+    runtime_machine_indexed_string_runtime_frame_address_offset,
+    string_descriptor_machine_address_offset, string_descriptor_pointee_address_offset,
+};
+use super::context::InstructionRelocationContext;
+use omega_target_operations::SelectedInstructionKind;
+
+pub(super) fn collect_runtime_storage_string_relocations(
+    context: &mut InstructionRelocationContext<'_, '_>,
+    instruction: &SelectedInstructionKind,
+) -> bool {
+    match instruction {
+        SelectedInstructionKind::WriteRuntimeMachineString { data, .. } => {
+            let data_symbol = context.data_object_symbol_handle(*data);
+            context.insert_data_address_at_instruction_start(data_symbol);
+            context.insert_data_address_at_relative_offset(
+                string_descriptor_machine_address_offset(context.input.target.architecture),
+                context.machine_storage_symbol_handle(),
+            );
+            true
+        }
+        SelectedInstructionKind::WriteRuntimePointeeString { data, .. } => {
+            let data_symbol = context.data_object_symbol_handle(*data);
+            context.insert_data_address_at_instruction_start(data_symbol);
+            context.insert_data_address_at_relative_offset(
+                string_descriptor_pointee_address_offset(context.input.target.architecture),
+                context.runtime_frame_symbol_handle(),
+            );
+            true
+        }
+        SelectedInstructionKind::WriteRuntimeFrameIndexedString {
+            data,
+            element_byte_size,
+            field_byte_offset,
+            ..
+        } => {
+            context.insert_data_address_at_instruction_start(context.runtime_frame_symbol_handle());
+            let data_symbol = context.data_object_symbol_handle(*data);
+            context.insert_data_address_at_relative_offset(
+                runtime_frame_indexed_string_data_address_offset(
+                    context.input.target.architecture,
+                    *element_byte_size,
+                    *field_byte_offset,
+                ),
+                data_symbol,
+            );
+            true
+        }
+        SelectedInstructionKind::WriteRuntimeMachineIndexedString {
+            data,
+            base_byte_offset,
+            element_byte_size,
+            field_byte_offset,
+            ..
+        } => {
+            context
+                .insert_data_address_at_instruction_start(context.machine_storage_symbol_handle());
+            context.insert_data_address_at_relative_offset(
+                runtime_machine_indexed_string_runtime_frame_address_offset(
+                    context.input.target.architecture,
+                    *base_byte_offset,
+                ),
+                context.runtime_frame_symbol_handle(),
+            );
+            let data_symbol = context.data_object_symbol_handle(*data);
+            context.insert_data_address_at_relative_offset(
+                runtime_machine_indexed_string_data_address_offset(
+                    context.input.target.architecture,
+                    *base_byte_offset,
+                    *element_byte_size,
+                    *field_byte_offset,
+                ),
+                data_symbol,
+            );
+            true
+        }
+        _ => false,
+    }
+}
