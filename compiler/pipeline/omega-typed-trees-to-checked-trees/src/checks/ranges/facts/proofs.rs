f77d03d6554f3f3b86c5f10e22404dd09fedff18
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 10:22:09 -0700

    Split range proof alias propagation

--- compiler/pipeline/omega-typed-trees-to-checked-trees/src/checks/ranges/facts/proofs.rs
+++ compiler/pipeline/omega-typed-trees-to-checked-trees/src/checks/ranges/facts/proofs.rs
@@ -1,81 +1,8 @@
 use super::RangeFacts;
 
+mod aliases;
+
 impl RangeFacts<'_> {
-    pub(in crate::checks::ranges) fn alias_collection(&mut self, original: &str, alias: &str) {
-        if original == alias {
-            return;
-        }
-
-        for (_, index) in self
-            .proven_indexes
-            .clone()
-            .into_iter()
-            .filter(|(collection, _)| collection == original)
-        {
-            self.prove_index(alias.to_owned(), index);
-        }
-        for (_, bound) in self
-            .proven_range_bounds
-            .clone()
-            .into_iter()
-            .filter(|(collection, _)| collection == original)
-        {
-            self.prove_range_bound(alias.to_owned(), bound);
-        }
-        if let Some(minimum_length) = self.minimum_length(original) {
-            self.prove_minimum_length(alias.to_owned(), minimum_length);
-        }
-    }
-
-    pub(in crate::checks::ranges) fn alias_index(&mut self, original: &str, alias: &str) {
-        if original == alias {
-            return;
-        }
-
-        for (_, upper_bound) in self
-            .proven_index_upper_bounds
-            .clone()
-            .into_iter()
-            .filter(|(index, _)| index == original)
-        {
-            self.prove_index_upper_bound(alias.to_owned(), upper_bound);
-        }
-        for (collection, _) in self
-            .proven_indexes
-            .clone()
-            .into_iter()
-            .filter(|(_, index)| index == original)
-        {
-            self.prove_index(collection, alias.to_owned());
-        }
-        for (collection, _) in self
-            .proven_range_bounds
-            .clone()
-            .into_iter()
-            .filter(|(_, bound)| bound == original)
-        {
-            self.prove_range_bound(collection, alias.to_owned());
-        }
-        for (lower, upper) in self
-            .proven_orderings
-            .clone()
-            .into_iter()
-            .filter(|(lower, upper)| lower == original || upper == original)
-        {
-            let lower = if lower == original {
-                alias.to_owned()
-            } else {
-                lower
-            };
-            let upper = if upper == original {
-                alias.to_owned()
-            } else {
-                upper
-            };
-            self.prove_at_most(lower, upper);
-        }
-    }
-
     pub(in crate::checks::ranges) fn prove_index(&mut self, collection: String, index: String) {
         if !self
             .proven_indexes
