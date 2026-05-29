f00ff940f403a87d5696e94a6cf73d667cf309e1
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 16:49:22 -0700

    Cover operation semantic query defaults

--- compiler/foundation/omega-core/src/operations.rs
+++ compiler/foundation/omega-core/src/operations.rs
@@ -34,3 +34,46 @@ pub trait OperationSemanticQuery {
         self.semantic_domain().touches_runtime_storage()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{OperationDomain, OperationSemanticQuery};
+
+    struct TestOperation {
+        domain: OperationDomain,
+    }
+
+    impl OperationSemanticQuery for TestOperation {
+        fn semantic_domain(&self) -> OperationDomain {
+            self.domain
+        }
+    }
+
+    #[test]
+    fn runtime_storage_domains_are_centralized() {
+        assert!(OperationDomain::GuardEvaluation.touches_runtime_storage());
+        assert!(OperationDomain::RuntimeTextAssembly.touches_runtime_storage());
+        assert!(OperationDomain::RuntimeRead.touches_runtime_storage());
+        assert!(OperationDomain::RuntimeWrite.touches_runtime_storage());
+        assert!(OperationDomain::RuntimeCopy.touches_runtime_storage());
+
+        assert!(!OperationDomain::FunctionBoundary.touches_runtime_storage());
+        assert!(!OperationDomain::DispatchControl.touches_runtime_storage());
+        assert!(!OperationDomain::HostBoundary.touches_runtime_storage());
+    }
+
+    #[test]
+    fn semantic_query_defaults_follow_domain_rules() {
+        let host = TestOperation {
+            domain: OperationDomain::HostBoundary,
+        };
+        let copy = TestOperation {
+            domain: OperationDomain::RuntimeCopy,
+        };
+
+        assert!(host.crosses_host_boundary());
+        assert!(!host.touches_runtime_storage());
+        assert!(!copy.crosses_host_boundary());
+        assert!(copy.touches_runtime_storage());
+    }
+}
