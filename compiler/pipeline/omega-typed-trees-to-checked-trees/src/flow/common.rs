4bb523478ab7c9ee825f3c2a0efb10ab911401af
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 09:44:24 -0700

    Rename flow common helper modules

--- compiler/pipeline/omega-typed-trees-to-checked-trees/src/flow/common.rs
+++ compiler/pipeline/omega-typed-trees-to-checked-trees/src/flow/common.rs
@@ -1,12 +1,12 @@
-mod arenas;
-mod lookup;
+mod fact_lookup;
+mod spans;
 
 use super::*;
-pub(crate) use arenas::{
+pub(crate) use fact_lookup::{
+    borrow_state_fact, effects_call, effects_machine, effects_state, proof_contract_call,
+};
+pub(crate) use spans::{
     append_constraint_ref, append_flow_contexts_for_points, append_place_segments,
     append_semantic_constraints_for_points, appended_span_since, clone_constraint_refs,
     clone_flow_contexts, filter_constraint_refs, project_constraint_refs_to_active_contexts,
-};
-pub(crate) use lookup::{
-    borrow_state_fact, effects_call, effects_machine, effects_state, proof_contract_call,
 };
