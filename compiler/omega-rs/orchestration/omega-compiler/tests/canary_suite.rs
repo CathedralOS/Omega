63c8bd010990d3f9801e8f265240f611bc89a7ff
Zachary Canann <zacharycanann@gmail.com>
2026-08-12 07:08:00 +0200

    corpus: bind build evaluator program entry

--- compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
@@ -43712,6 +43712,10 @@ fn migrated_main_entries_are_selected_only_through_their_target_root_bindings() 
             "linux_x64",
         ),
         (
+            "build/static_machine_parameter_config_compile",
+            "windows_x64",
+        ),
+        (
             "text/runtime_x86_general_double_indexed_string_concat_compile",
             "linux_x64",
         ),
