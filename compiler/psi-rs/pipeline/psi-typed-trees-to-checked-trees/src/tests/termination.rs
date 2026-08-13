f21f79baa80976dcd25065f8bdaa25f142ae85c3
Zachary Canann <zacharycanann@gmail.com>
2026-08-13 03:38:46 +0200

    calls: compose aliased slice statement arguments

--- compiler/psi-rs/pipeline/psi-typed-trees-to-checked-trees/src/tests/termination.rs
+++ compiler/psi-rs/pipeline/psi-typed-trees-to-checked-trees/src/tests/termination.rs
@@ -5794,6 +5794,48 @@ fn transparent_returned_place_accepts_bounded_indexed_statement_arguments() {
         result
     }
 
+    machine return_after_alias_slice_view_indexed_statement<
+        'cells, 'result, 'write
+    >(
+        cells: &'cells mut [u64; 2],
+        result: &'result mut u64,
+        index_write: &'write mut u64
+    ) -> &'result mut u64 {
+        let alias: &mut [u64; 2] = cells;
+        write_argument(
+            &mut alias.as_mut_slice()[
+                identity_index(write_index(index_write))
+            ]
+        );
+        result
+    }
+
+    machine return_after_deep_alias_slice_view_indexed_statement<
+        'cells, 'result
+    >(
+        cells: &'cells mut [u64; 2],
+        result: &'result mut u64
+    ) -> &'result mut u64 {
+        let alias: &mut [u64; 2] = cells;
+        write_argument(
+            &mut alias.as_mut_slice()[
+                identity_index(identity_index(make_index()))
+            ]
+        );
+        result
+    }
+
+    machine return_after_recursive_alias_slice_view_indexed_statement<
+        'cells, 'result
+    >(
+        cells: &'cells mut [u64; 2],
+        result: &'result mut u64
+    ) -> &'result mut u64 {
+        let alias: &mut [u64; 2] = cells;
+        write_argument(&mut alias.as_mut_slice()[recursive_index()]);
+        result
+    }
+
     machine return_after_helper_slice_view_indexed_statement<
         'cells, 'result, 'write
     >(
@@ -6068,6 +6110,31 @@ fn transparent_returned_place_accepts_bounded_indexed_statement_arguments() {
         alias = 3;
     }
 
+    machine Main::alias_slice_view_indexed_statement_result(&mut self) {
+        let alias: &mut u64 = return_after_alias_slice_view_indexed_statement(
+            &mut self.cells,
+            &mut self.result,
+            &mut self.index_write
+        );
+        alias = 3;
+    }
+
+    machine Main::deep_alias_slice_view_indexed_statement_result(&mut self) {
+        let alias: &mut u64 = return_after_deep_alias_slice_view_indexed_statement(
+            &mut self.cells,
+            &mut self.result
+        );
+        alias = 3;
+    }
+
+    machine Main::recursive_alias_slice_view_indexed_statement_result(&mut self) {
+        let alias: &mut u64 = return_after_recursive_alias_slice_view_indexed_statement(
+            &mut self.cells,
+            &mut self.result
+        );
+        alias = 3;
+    }
+
     machine Main::helper_slice_view_indexed_statement_result(&mut self) {
         let alias: &mut u64 = return_after_helper_slice_view_indexed_statement(
             &mut self.cells,
@@ -6257,6 +6324,10 @@ fn transparent_returned_place_accepts_bounded_indexed_statement_arguments() {
             vec!["self.cells", "self.index_write", "self.result"],
         ),
         (
+            "Main::alias_slice_view_indexed_statement_result",
+            vec!["self.cells", "self.index_write", "self.result"],
+        ),
+        (
             "Main::helper_slice_view_indexed_statement_result",
             vec!["self.cells", "self.index_write", "self.result"],
         ),
@@ -6325,6 +6396,8 @@ fn transparent_returned_place_accepts_bounded_indexed_statement_arguments() {
         "Main::recursive_indexed_statement_result",
         "Main::deep_slice_view_indexed_statement_result",
         "Main::recursive_slice_view_indexed_statement_result",
+        "Main::deep_alias_slice_view_indexed_statement_result",
+        "Main::recursive_alias_slice_view_indexed_statement_result",
         "Main::deep_helper_slice_view_indexed_statement_result",
         "Main::recursive_helper_slice_view_indexed_statement_result",
         "Main::recursive_helper_indexed_statement_result",
