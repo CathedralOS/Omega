7cef3afcce4b01eaff9ee0e10769cb3c7abe484d
Zachary Canann <zacharycanann@gmail.com>
2026-08-16 00:13:25 +0200

    tests: root wire field identity canary

--- compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
@@ -45186,6 +45186,7 @@ const ROOTED_BACKEND_PASS_CANARIES: &[&str] = &[
     "data/record_pattern_bind_all_exit",
     "data/record_pattern_double_underscore_field",
     "memory/repr_native_stable_layout",
+    "wire/wire_data_field_numbers",
     "traits/equatable_sum_stale_payload_exit",
     "traits/ring_requirement_satisfies_exit",
     "traits/runtime_trait_default_dispatch_exit",
