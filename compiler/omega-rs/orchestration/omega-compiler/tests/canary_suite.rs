134a07ebf162a58b1e4660e9865dea6474977240
Zachary Canann <zacharycanann@gmail.com>
2026-08-15 23:59:18 +0200

    tests: root native repr canary

--- compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
@@ -45185,6 +45185,7 @@ const ROOTED_BACKEND_PASS_CANARIES: &[&str] = &[
     "control_flow/runtime_multi_assignment_value_calls",
     "data/record_pattern_bind_all_exit",
     "data/record_pattern_double_underscore_field",
+    "memory/repr_native_stable_layout",
     "traits/equatable_sum_stale_payload_exit",
     "traits/ring_requirement_satisfies_exit",
     "traits/runtime_trait_default_dispatch_exit",
