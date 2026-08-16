850234b01fd7c7fb9f3b1899bb62d92348244456
Zachary Canann <zacharycanann@gmail.com>
2026-08-16 04:56:42 +0200

    tests: root nested branch value canary

--- compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
@@ -45187,6 +45187,7 @@ const ROOTED_BACKEND_PASS_CANARIES: &[&str] = &[
     "control_flow/runtime_compare_pair_dispatch_exit",
     "control_flow/runtime_local_boolean_or_value_exit",
     "control_flow/runtime_multi_assignment_value_calls",
+    "control_flow/runtime_nested_branch_value",
     "control_flow/runtime_negated_boolean_place_guard_exit",
     "control_flow/runtime_negated_comparison_guard_exit",
     "control_flow/runtime_nonplace_record_pattern_single_evaluation_exit",
