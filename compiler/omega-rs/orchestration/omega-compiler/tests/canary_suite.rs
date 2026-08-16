39e7313c897c0f9b00dfd4197382745c3f985fb3
Zachary Canann <zacharycanann@gmail.com>
2026-08-16 07:47:46 +0200

    tests: root trapping shift canary

--- compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
+++ compiler/omega-rs/orchestration/omega-compiler/tests/canary_suite.rs
@@ -21650,13 +21650,8 @@ fn trapping_shift_count_traps_aborts() {
     let build_dir = std::env::temp_dir().join(format!("omega-trap-shcnt-{}", std::process::id()));
     let _ = fs::remove_dir_all(&build_dir);
 
-    compile(CompileOptions {
-        root_path: canary.join("main.omg"),
-        build_dir: Some(build_dir.clone()),
-        target_name: None,
-        write_output: true,
-    })
-    .expect("trapping shift-count canary should compile");
+    compile_rooted_canary_for_native_host(&canary, build_dir.clone())
+        .expect("trapping shift-count canary should compile");
 
     let output = Command::new(build_dir.join(executable_name()))
         .output()
