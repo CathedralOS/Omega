c4ac0efa2504a3dc950f4b3b4c42d76c9dba0b12
Zachary Canann <zacharycanann@gmail.com>
2026-08-21 22:40:46 -0700

    Separate landed cast alias bounds

--- compiler/psi-rs/pipeline/psi-checked-trees-to-terminal/src/nonzero_divisor_certificate/alias_transport/cast/literal/completion.rs
+++ compiler/psi-rs/pipeline/psi-checked-trees-to-terminal/src/nonzero_divisor_certificate/alias_transport/cast/literal/completion.rs
@@ -1,10 +1,11 @@
 //! Typed landed-literal alias completion for exact integer casts.
 
 use psi_core::{Proposition, PropositionContext, ScalarTerm};
-use psi_proof_kernel::{ProofNode, ProofRule};
+use psi_proof_kernel::ProofNode;
 
 use super::super::super::super::cast_custody;
-use super::super::super::super::integer_evidence::closed_integer_relation;
+
+mod bound;
 
 #[allow(clippy::too_many_arguments)]
 pub(super) fn prove(
@@ -34,37 +35,16 @@ pub(super) fn prove(
         else {
             continue;
         };
-        let closed_relation = if endpoint == 1 {
-            Proposition::LessOrEqual(source_endpoint.clone(), landed_literal.clone())
-        } else {
-            Proposition::LessOrEqual(landed_literal.clone(), source_endpoint.clone())
-        };
-        let Some(closed_relation) = closed_integer_relation(closed_relation) else {
+        let Some(root_bound) = bound::prove(
+            root,
+            alias,
+            landed_literal,
+            source_endpoint,
+            endpoint,
+            root_equality.clone(),
+            literal_equality.clone(),
+        ) else {
             continue;
-        };
-        let alias_bound = ProofNode {
-            conclusion: if endpoint == 1 {
-                Proposition::LessOrEqual(source_endpoint.clone(), alias.clone())
-            } else {
-                Proposition::LessOrEqual(alias.clone(), source_endpoint.clone())
-            },
-            rule: ProofRule::IntegerLessOrEqualSubstitution {
-                relation: Box::new(closed_relation),
-                equality: Box::new(literal_equality.clone()),
-                endpoint,
-            },
-        };
-        let root_bound = ProofNode {
-            conclusion: if endpoint == 1 {
-                Proposition::LessOrEqual(source_endpoint, root.clone())
-            } else {
-                Proposition::LessOrEqual(root.clone(), source_endpoint)
-            },
-            rule: ProofRule::IntegerLessOrEqualSubstitution {
-                relation: Box::new(alias_bound),
-                equality: Box::new(root_equality.clone()),
-                endpoint,
-            },
         };
         if let Some(proof) = cast_custody::prove_from_root(
             context,
